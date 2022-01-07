3acd69da94aa9011d2b7b5ef4232f627cbc66db9
SlavikSumish <88457082+SlavikSumish@users.noreply.github.com>
2022-01-07 18:23:53 +0200

    Add files via upload

---
+++ src/js/01-gallery.js
@@ -0,0 +1,39 @@
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+}
+
+refs.gallery.addEventListener('click', onImageClick)
+
+function onImageClick(e) {
+  e.preventDefault();
+  const isClickOnImage = e.target.classList.contains('gallery__image');
+  if (!isClickOnImage) {
+    return;
+  }
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description}) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`
+}).join('');
+}
+
+refs.gallery.innerHTML = galleryMarkup;
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250 });
+
+console.log(galleryItems);
